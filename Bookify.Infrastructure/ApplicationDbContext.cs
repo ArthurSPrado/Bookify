ed0a1e79c1b8774e5162a031d1970530437c601f
ArthurSPrado <arthurhsprado@outlook.com>
2024-08-19 16:47:36 -0300

    Adição de entidades e configurações iniciais da camada de infra

---
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+using Bookify.Domain.Abstractions;
+using Microsoft.EntityFrameworkCore;
+
+namespace Bookify.Infrastructure;
+
+public sealed class ApplicationDbContext : DbContext, IUnityOfWork
+{
+    public ApplicationDbContext(DbContextOptions options)
+        : base(options)
+    {
+    }
+}
