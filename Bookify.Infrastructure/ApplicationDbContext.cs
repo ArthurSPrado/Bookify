29387a56e8449c807fbea1f11a5c4e48285c7ff3
ArthurSPrado <arthurhsprado@outlook.com>
2024-08-19 17:57:58 -0300

    Adição de classe generica de repository, implementação de alguns repositories e SqlConectionFactory

--- Bookify.Infrastructure/ApplicationDbContext.cs
+++ Bookify.Infrastructure/ApplicationDbContext.cs
@@ -9,4 +9,10 @@ public sealed class ApplicationDbContext : DbContext, IUnityOfWork
         : base(options)
     {
     }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(ApplicationDbContext).Assembly);
+        base.OnModelCreating(modelBuilder);
+    }
 }
---
+++ Bookify.Infrastructure/Repositories/UserRepository.cs
@@ -0,0 +1,11 @@
+using Bookify.Domain.Users;
+using Bookify.Domain.Users.Interfaces;
+
+namespace Bookify.Infrastructure.Repositories;
+
+internal sealed class UserRepository : Repository<User>, IUserRepository
+{
+    public UserRepository(ApplicationDbContext dbContext) : base(dbContext)
+    {
+    }
+}
